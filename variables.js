e986e5337488db71231658dd6101e3de4f92fffe
Susan Belén <152219079+susanynz@users.noreply.github.com>
2024-01-08 17:46:25 -0600

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,49 @@
+//ejemplos de variables numéricas
+
+var edad = 28;
+var temperaturaGdl = 20;
+var numeroTelefonico = 3334488832;
+var numeroTotaldeSemanas = 52;
+var kmCorridosEnUnaSemana = 45;
+var peso = 67;
+var precioTransportePublico = 9.50;
+var altura = 1.61;
+var temperaturaCorporal= 37;
+var numeroDeLaSuerte= 7;
+
+
+//ejemplos de variables string
+
+var nombre = "Susan";
+var apellido = "Yáñez";
+var ciudad = "Guadalajara";
+var signoZodiacal = "cáncer";
+var frutaFavorita= "manzana";
+var hobbie = "knitting";
+var ultimoLibroLeido = "Correr en femenino";
+var color = "azul";
+var marcaCoche = "Toyota";
+var profesion = "programador"
+
+
+//ejemplos de variables booleanas
+var miNombreEsSusan = true;
+var elClimaEstaCalido = false;
+var esTemporadaVacacional = false;
+var tengoNumeroTelefónico = true;
+var usuarioAutenticado = true;
+var caeNieveEnGdl = false;
+var esTemporadaDeLluvia = false;
+var laActividadEsSencilla = false;
+var soyDeGdl = true;
+var meGustaTejer = true;
+
+
+//operaciones aritmeticas con datos numéricos
+ + numeroDeLaSuerte; 
+console.log("El numero mágico es", edad+ numeroDeLaSuerte);
+
+var resultadoMultiplicacion= numeroTotaldeSemanas * kmCorridosEnUnaSemana; 
+console.log("En promedio corres al año",numeroTotaldeSemanas * kmCorridosEnUnaSemana);
+
+

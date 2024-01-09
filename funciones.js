ce19ba6750a8c53849a94cb0ca80db46b66532a8
Susan Belén <152219079+susanynz@users.noreply.github.com>
2024-01-09 15:37:06 -0600

    Add files via upload

---
+++ funciones.js
@@ -0,0 +1,15 @@
+
+//calcular el perimetro de un circulo
+function perimetroCirculo(radioCirculo) {
+    return `El perimetro de un círculo es ${2 * 3.1416 * radioCirculo }`
+}
+
+ //calcular el area de un rectangulo
+function perimetrorectangulo (base, _altura){
+        return `El perimetro de un rectángulo es ${(2 * base) + (2 * _altura)}`
+}
+
+//calcular el cuadrado de un numero
+function cuadradoNumerico (numero){
+    return `El cuadrado de ${numero} es ${numero * numero}`
+}
